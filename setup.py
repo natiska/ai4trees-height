4b51bea7d00c808e2417a6aad3f8cd023a780b24
Natalia Skaczkowska-Drabczyk <natalia.skaczkowska@gmail.com>
2022-08-02 04:30:41 +0000

    Initial project structure by Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identifying trees based the Digital Elevation Model provided by drone images',
+    author='FruitPunchAI',
+    license='',
+)
